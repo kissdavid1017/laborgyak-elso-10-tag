7882f294da7ccc56491fabfd9a3517760e4afeb3
kissdavid1017 <55405306+kissdavid1017@users.noreply.github.com>
2019-11-06 19:44:19 +0100

    Add files via upload

--- laborgyak.cpp
+++ laborgyak.cpp
@@ -1,37 +1,43 @@
 #include<std_lib_facilities.h>
+#include<fstream>
 using namespace std;
 int main()
 {
-  char c,fn[10];
+  char file[10];
   int n=0;
+  char c;
   vector<char> v;
   cout<<"Ird be a file nevet:";
-  cin>>fn;
-  ifstream be(fn);
+  cin>>file;
+  ifstream be(file);
+
   if(!be)
   {
-    cout<<"Nincs meg a file";
+      cout<<"Nincs meg a file"; return 0;
 
-    return 0;
   }
 
-  while(be.eof()==0)
+  while(n<=10*1024*1024)
   {
+      n++;
+      be>>c;
+      v.push_back(c);
+  }
 
-
-    while(n<=10)
-    {
-        n++;
-
-    v.push_back(c);
-    for(int i=0;i<n;i++)
+  for(int i=0;i<n;i++)
     {
         be.get(c);
         cout<<v[i];
-    }
+
+
     }
 
-  }
+  return 0;
 
-  getchar();
+
+
+
+
+
+
 }
